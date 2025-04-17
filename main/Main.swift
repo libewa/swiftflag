5c7b038cb3445af912766033d8b5c2b1f324a5f1
Linus Warnatz <linus@libewa.xyz>
2025-04-17 14:59:54 +0200

    build using own `app_main` function C++ code can't find `setup` and `loop` for some reason

--- main/Main.swift
+++ main/Main.swift
@@ -6,17 +6,23 @@ let FLAG_W: Int32 = 70
 let FLAG_H: Int32 = 50
 var loopcount = Int32(0)
 
-@_cdecl("setup")
 func setup() {
     print("Hello, World!")
     tft.`init`()
     tft.setRotation(1)
 }
 
-@_cdecl("loop")
 func loop() {
     tft.fillScreen(UInt32(TFT_BLACK))
     waveFlag(.enby, on: &tft, flagX: FLAG_X, flagY: FLAG_Y, width: FLAG_W, height: FLAG_H, xmod: 0.1, sinmod: 3, loopcount: loopcount)
 	delay(100)
 	loopcount += 5
+}
+
+@_cdecl("app_main")
+func main() {
+    setup()
+    while true {
+        loop()
+    }
 }
