ab92c17d197e00720c3750460c1f2f776d21fe0f
Linus Warnatz <linus@libewa.xyz>
2025-07-15 17:34:31 +0200

    remove references to TFT_eSPI for presentability

--- main/Main.swift
+++ main/Main.swift
@@ -1,29 +1,12 @@
-/*
-var tft = TFT_eSPI()
-
-extension TFT_eSPI {
-    init() {
-        self.init(Int16(TFT_WIDTH), Int16(TFT_HEIGHT))
-    }
-}
-
-let FLAG_X: Int32 = 10
-let FLAG_Y: Int32 = 10
-let FLAG_W: Int32 = 70
-let FLAG_H: Int32 = 50
-*/
-var loopcount = Int32(0)
 var currentTone = 0
 var isFirstLoop = true
 
 var currentToneStart = UInt(0)
-var lastFlagUpdate = UInt(0)
 
 let speakerPin: UInt8 = 1
 
 func setup() {
     print("setup() called...")
-    //tft.`init`()
     pinMode(pin: speakerPin, mode: .output)
     delay(100)
     noTone(speakerPin)
@@ -42,12 +25,6 @@ func loop() {
         currentToneStart = millis()
         print("Starting \(tones[currentTone].duration.rawValue)ms tone \(tones[currentTone].freq)Hz")
         tone(speakerPin, tones[currentTone].freq.rawValue)
-    }
-    if millis() - lastFlagUpdate > 200 {
-        lastFlagUpdate = millis()
-        //tft.fillScreen(UInt32(TFT_BLACK))
-        //waveFlag(.enby, flagX: FLAG_X, flagY: FLAG_Y, width: FLAG_W, height: FLAG_H, xmod: 0.1, sinmod: 3, loopcount: loopcount)
-        loopcount += 5
     }
 }
 
